ea1f128dceea257868c1b3e66888b4476a524e48
ancipet <as50674@fer.hr>
2019-11-05 16:33:41 +0100

    Dodan WebshopController i stranica WebshopStart.cshtml. U navigacijskoj traci dodan link na Webshop stranicu.

---
+++ RudesWebapp/Controllers/WebshopController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RudesWebapp.Controllers
+{
+    public class WebshopController : Controller
+    {
+        public IActionResult WebshopStart()
+        {
+            return View();
+        }
+    }
+}
