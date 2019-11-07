82465805eba16900c5d483eea8be93de4bab7fc5
Danica <Dora@LAPTOP-K013DUPD>
2019-11-07 17:38:24 +0100

    Dodana testna funkcionalnost koja simulira login i logout.

---
+++ RudesWebapp/wwwroot/js/navbar.js
@@ -0,0 +1,13 @@
+﻿var navbar = new Vue({
+    el: "#navbar",
+    data: {
+        loggedIn: false,
+        dummyUsername: "divGljiva"
+    },
+    methods: {
+        switchLoginStatus: function () {
+            this.loggedIn = !this.loggedIn;
+        },
+        
+    }
+});
