24f44330907c122721ebc16e2ffeeb01e9dace6a
Frle <Ivan.Skorupan@fer.hr>
2019-11-07 16:18:19 +0100

    Dodana stranica za registraciju, dodane prikladne metode u HomeController i priprema linkova u navigacijskoj traci za Login i Register.

--- RudesWebapp/Controllers/HomeController.cs
+++ RudesWebapp/Controllers/HomeController.cs
@@ -33,6 +33,16 @@ namespace RudesWebapp.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
