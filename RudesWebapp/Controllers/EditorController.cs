1baa0ba8ea654921ffe8dc9f2c82a0324459284e
ftodoric <filip.todoric@fer.hr>
2020-01-04 17:58:31 +0100

    Grid layout on Index page fixed. Editor controller added. Editor/PlayerEditor view added for player data modification.

---
+++ RudesWebapp/Controllers/EditorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RudesWebapp.Controllers
+{
+    public class EditorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PlayerEditor()
+        {
+            return View();
+        }
+    }
+}
