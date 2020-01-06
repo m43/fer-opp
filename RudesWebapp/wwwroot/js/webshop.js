eeaad2023c3da16a6834b5ec00ac1fc8bbeab39d
ancipet <as50674@fer.hr>
2020-01-06 01:27:19 +0100

    Alignment issues fixed, styled buttons, filters and article images. Ivan created webshop.js file

---
+++ RudesWebapp/wwwroot/js/webshop.js
@@ -0,0 +1,29 @@
+﻿function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+var webshop = new Vue({
+    el: "#webshop",
+    data: {
+        allArticles: []
+    },
+    methods: {
+        getArticles: function () {
+            axios.get("/Webshop/GetArticles")
+                .then(response => {
+                    this.allArticles = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    },
+    beforeMount: function () {
+        this.getArticles();
+    }
+});
