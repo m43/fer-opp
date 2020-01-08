9e5fb1fe612eb637192d327bd354423ae9d608b6
pbubica <pb50564@fer.hr>
2020-01-08 23:51:09 +0100

    Added a DTO for Item and User.

---
+++ RudesWebapp/Dtos/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RudesWebapp.Dtos
+{
+    public class UserDTO
+    {
+        [PersonalData]
+        public string Name { get; set; } // TODO or is it [ProtectedPersonalData]
+        [PersonalData] 
+        public string LastName { get; set; }
+    }
+}
