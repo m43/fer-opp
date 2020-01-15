015b7aa652f913f88bef77e6fd0d2688649dc2e8
ancipet <as50674@fer.hr>
2020-01-15 13:02:41 +0100

    Now size radiobuttons in articleDetailsModal are disabled if there is no available article. For now, the list where quantity of available articles is hardcoded so i can see if the method is working since the real one is currently empty.

--- RudesWebapp/wwwroot/js/articleDetails.js
+++ RudesWebapp/wwwroot/js/articleDetails.js
@@ -1,9 +1,32 @@
 ﻿var articleDetailsModal = new Vue({
     el: "#articleDetailsModal",
     data: {
-        selectedArticle: {}
+        selectedArticle: {},
+        probneDostupneVelicine: [3, 0, 5, 0, 7],
+        kolicina1: 3,
+        kolicina2: 0
+        //kolicina
     },
-    methods: {
+  
+    computed: {
+        isDisabled1: function () {
+            return (this.kolicina1 <= 0);
+        },
+        isDisabled2: function () {
+            return (this.kolicina2 <= 0);
+        },
+        isDisabled: function () {
+            return true;
+        },
+        isNotDisabled: function () {
+            return false;
+        }
 
+       /*
+        isDisabled: function(){
+            return (this.kolicina <=0);
+        }
+        */
     }
+
 });
